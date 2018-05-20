107510b9dd7b97e8a5364895687dbbaac83aeaba
Fledron <aago2001@web.de>
2018-05-20 23:03:46 +0200

    Update TaskHandler.cs
    Fixed problems and enabled task performing forever, but when the task isn't running anymore it automatically switches to the next one.

--- TaskHandler/TaskHandler.cs
+++ TaskHandler/TaskHandler.cs
@@ -6,28 +6,63 @@ using GTA.Native;
 
 namespace TestModForTesting
 {
-    class TaskHandler
+    class TaskHandlerTwo
     {
-        Ped ped;
+        public Ped ped;
 
-        public TaskHandler(Ped p)
+        public TaskHandlerTwo(Ped p)
         {
             ped = p;
         }
 
-        Hash TaskNow;
-        object[] ArgsNow;
+        private Hash TaskNow;
+        private object[] ArgsNow;
+        private int SequenceNow;
 
-        List<TaskNTime> tasks = new List<TaskNTime>();
-        public static List<TaskHandler> handlers = new List<TaskHandler>();
+        public List<TaskNTimeTwo> tasks = new List<TaskNTimeTwo>();
+        private static List<TaskHandlerTwo> handlers = new List<TaskHandlerTwo>();
 
+        public static List<TaskHandlerTwo> GetTaskHandlers()
+        {
+            return handlers;
+        }
 
+        public static TaskHandlerTwo GetTaskHandler(Ped p)
+        {
+            foreach (TaskHandlerTwo t in handlers)
+            {
+                if (t.ped == p)
+                {
+                    return t;
+                }
+            }
+            return AddTaskHandler(p);
+        }
+
+        public static TaskHandlerTwo AddTaskHandler(Ped p)
+        {
+            TaskHandlerTwo t = null;
+            foreach (TaskHandlerTwo th in handlers)
+            {
+                if (th.ped == p)
+                {
+                    t = th;
+                }
+            }
+            if (t == null)
+            {
+                t = new TaskHandlerTwo(p);
+            }
+            handlers.Add(t);
+            return t;
+        }
 
         public void PerformTick()
         {
+
             if (GetPendingTasks() > 0)
             {
-                TaskNTime t = tasks[0];
+                TaskNTimeTwo t = tasks[0];
                 if (ArgsNow == null || TaskNow != t.Task || ArgsNow != t.Args)
                 {
                     TaskNow = t.Task;
@@ -37,17 +72,36 @@ namespace TestModForTesting
                     {
                         InArgs.Add(new InputArgument(o));
                     }
-                    Function.Call(Hash.CLEAR_PED_TASKS_IMMEDIATELY, ped);
+                    OutputArgument Sequence = new OutputArgument();
+                    Function.Call(Hash.OPEN_SEQUENCE_TASK, Sequence);
+                    int SeqID = Sequence.GetResult<int>();
+                    Function.Call(Hash.CLEAR_PED_TASKS_IMMEDIATELY, 0);
                     Function.Call(t.Task, InArgs.ToArray());
-                    //UI.Notify("Performing "+t.Task.ToString());
+                    Function.Call(Hash.TASK_STAND_STILL, 0, 2000);
+                    Function.Call(Hash.CLOSE_SEQUENCE_TASK, SeqID);
+                    Function.Call(Hash.TASK_PERFORM_SEQUENCE, ped, SeqID);
+                    Function.Call(Hash.CLEAR_SEQUENCE_TASK, Sequence);
+                    SequenceNow = SeqID;
+                    //UI.Notify("Performing " + t.Task);
+                    t.StartTime = Game.GameTime;
                 }
-                if (Game.GameTime >= t.EndTime && t.EndTime != -1)
+                int Prog = Function.Call<int>(Hash.GET_SEQUENCE_PROGRESS, ped);
+                int RunningTime = Game.GameTime - t.StartTime;
+                if ((GetRemainingTime() != -1 && GetRemainingTime() <= 0)||(t.HowLong == -1 && Prog == 2 && RunningTime>=2000))
                 {
                     tasks.RemoveAt(0);
                     TaskNow = Hash.WAIT;
                     ArgsNow = null;
                 }
+
             }
+            else
+            {
+                //UI.ShowSubtitle("Progress: No Task");
+                Function.Call(Hash.CLEAR_PED_TASKS, ped);
+            }
+            int Progress = Function.Call<int>(Hash.GET_SEQUENCE_PROGRESS, ped);
+            //UI.ShowSubtitle(tasks.Count + " Progress: " + Progress);
         }
 
 
@@ -55,8 +109,8 @@ namespace TestModForTesting
         {
             if (GetPendingTasks() > 0)
             {
-                TaskNTime t = tasks[0];
-                return t.EndTime;
+                TaskNTimeTwo t = tasks[0];
+                return t.HowLong + t.StartTime;
             }
             else
             {
@@ -68,8 +122,7 @@ namespace TestModForTesting
         {
             if (GetPendingTasks() > 0)
             {
-                TaskNTime t = tasks[0];
-                return t.EndTime - Game.GameTime;
+                return GetTimeLeft(0);
             }
             else
             {
@@ -77,27 +130,26 @@ namespace TestModForTesting
             }
         }
 
-        public TaskNTime GetCurrentTask()
-        {
-            return new TaskNTime(TaskNow, GetRemainingTime(), ArgsNow);
-        }
-
         public Hash GetTask(int pos)
         {
             if (tasks.Count > pos)
             {
-                TaskNTime b = tasks[pos];
+                TaskNTimeTwo b = tasks[pos];
                 return b.Task;
             }
             return tasks[tasks.Count - 1].Task;
         }
 
-        public double GetTimeLeft(int pos)
+        public int GetTimeLeft(int pos)
         {
             if (tasks.Count > pos)
             {
-                TaskNTime b = tasks[pos];
-                return b.EndTime - Game.GameTime;
+                TaskNTimeTwo b = tasks[pos];
+                if (b.StartTime != -1 && b.HowLong != -1)
+                {
+                    return b.HowLong - (Game.GameTime - b.StartTime);
+                }
+                return -1;
             }
             return -1;
         }
@@ -110,7 +162,7 @@ namespace TestModForTesting
         public Hash[] GetAllTasksAsArray()
         {
             Hash[] t = { };
-            foreach (TaskNTime tnt in tasks)
+            foreach (TaskNTimeTwo tnt in tasks)
             {
                 t[t.Length - 1] = tnt.Task;
             }
@@ -125,7 +177,7 @@ namespace TestModForTesting
         public void ChangeTask(int pos, Hash newTask, int howLong, object[] args)
         {
             tasks.RemoveAt(pos);
-            tasks.Insert(pos, new TaskNTime(newTask, Game.GameTime + howLong, args));
+            tasks.Insert(pos, new TaskNTimeTwo(newTask, howLong, -1, args));
         }
 
         public void AddTask(Hash task, object[] args)
@@ -133,36 +185,43 @@ namespace TestModForTesting
             AddTask(task, -1, args);
         }
 
-        public void AddTask(Hash task, int howLong, object[] args)
+        public void AddTask(Hash task, int HowLong, object[] args)
         {
-            AddTask(task, int.MaxValue, howLong, args);
+            AddTask(task, int.MaxValue, HowLong, args);
         }
 
         public void AddTask(Hash task, int pos, int HowLong, object[] args)
         {
             if (tasks.Count > pos)
             {
-                tasks.Insert(pos, new TaskNTime(task, Game.GameTime + HowLong, args));
+                tasks.Insert(pos, new TaskNTimeTwo(task, HowLong, -1, args));
             }
             else
             {
-                tasks.Add(new TaskNTime(task, Game.GameTime + HowLong, args));
+                tasks.Add(new TaskNTimeTwo(task, HowLong, -1, args));
             }
         }
 
+        public void ClearTasks()
+        {
+            tasks.Clear();
+        }
 
     }
 
-    class TaskNTime
+    internal class TaskNTimeTwo
     {
         public Hash Task { get; set; }
         public object[] Args { get; set; }
-        public int EndTime { get; set; }
-        public TaskNTime(Hash Task, int EndTime, object[] Args)
+        public int StartTime { get; set; }
+        public int HowLong { get; set; }
+        public TaskNTimeTwo(Hash Task, int HowLong, int StartTime, object[] Args)
         {
             this.Task = Task;
-            this.EndTime = EndTime;
+            this.HowLong = HowLong;
+            this.StartTime = StartTime;
             this.Args = Args;
+
         }
 
     }
